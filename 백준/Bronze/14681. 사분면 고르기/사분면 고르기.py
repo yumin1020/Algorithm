75806cbd8239da51b476f3cdb6a1334d1b63ebe9
Yumin <166204685+yumin1020@users.noreply.github.com>
2025-02-27 21:12:43 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+import sys
+
+x, y = map(int, sys.stdin.read().split())
+
+if(x > 0 and y > 0):
+    print(1)
+elif(x < 0 and y > 0):
+    print(2)
+elif(x < 0 and y < 0):
+    print(3)
+elif(x > 0 and y < 0):
+    print(4)
